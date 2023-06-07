344f77f4a5e00204d097b19ba72efd783dd2c245
Caio Fábio Juvino de Queiroz <caio.queiroz@ccc.ufcg.edu.br>
2023-06-07 15:46:03 -0300

    Adicionando verificação de nome

--- src/Cliente.java
+++ src/Cliente.java
@@ -2,11 +2,22 @@ public class Cliente {
 
     private String nome;
 
+    public Cliente(String nome) throws Exception {
+        this.nome = checkNome(nome);
+    }
+
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setNome(String nome) throws Exception {
+        checkNome(nome);
+    }
+
+    public String checkNome(String nome) throws Exception {
+        if(nome == null || nome.isBlank()){
+            throw new Exception("Nome não pode ser vazio");
+        }
+        return nome;
     }
 }
